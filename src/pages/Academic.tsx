775f8462cec860421ac3a82f260ad92c41e2b7a6
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-19 17:48:35 +0900

    feat: init mdx and style

---
+++ src/pages/Academic.tsx
@@ -0,0 +1,11 @@
+import AcademicContent from "../content/academic.mdx";
+
+const Academic = () => {
+  return (
+    <div className="markdown m-4 mt-16 place-self-center">
+      <AcademicContent />
+    </div>
+  );
+};
+
+export default Academic;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Academic from "./Academic";
+
 function App() {
   return (
-    <>
-      <div className="text-4xl text-purple-400">
-        <h1>Academic page</h1>
+    <BrowserRouter>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Academic />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
