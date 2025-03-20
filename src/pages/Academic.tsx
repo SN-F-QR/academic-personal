b051fccb5b917f0ff1b0ec276b96d2087ab1b413
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-20 17:09:39 +0900

    fix: css style

--- src/pages/Academic.tsx
+++ src/pages/Academic.tsx
@@ -2,7 +2,7 @@ import AcademicContent from "../content/academic.mdx";
 
 const Academic = () => {
   return (
-    <div className="markdown flex h-full flex-col place-self-center pt-16 pb-20">
+    <div className="markdown flex h-full flex-col place-self-center pt-8 pb-20">
       <AcademicContent />
     </div>
   );
