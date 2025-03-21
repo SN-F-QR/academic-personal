33472e0490a3f7b373b2c46e0ac67db3a9b040d5
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-21 19:52:05 +0900

    feat: image manager

---
+++ src/utils/imageManager.ts
@@ -0,0 +1,21 @@
+const imageList = import.meta.glob("../assets/*.{jpg,jpeg,png}", {
+  query: "?url",
+  eager: true,
+  import: "default",
+});
+
+export const imagePaths = new Map<string, string[]>();
+
+export const loadImagePaths = () => {
+  for (const path in imageList) {
+    const fileName = path.split("/").pop()?.split("_")[0] as string;
+    const staticPath = imageList[path] as string;
+    if (imagePaths.has(fileName)) {
+      imagePaths.set(fileName, [...imagePaths.get(fileName)!, staticPath]);
+    } else {
+      imagePaths.set(fileName, [staticPath]);
+    }
+  }
+};
+
+loadImagePaths();
