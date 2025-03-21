2c86c0c394e1dbe83318ca3f36f63971ed7ec83f
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-21 19:43:31 +0900

    fix: min height

--- src/pages/Academic.tsx
+++ src/pages/Academic.tsx
@@ -2,7 +2,7 @@ import AcademicContent from "../content/academic.mdx";
 
 const Academic = () => {
   return (
-    <div className="markdown flex h-full flex-col place-self-center overflow-hidden pt-8 pb-20">
+    <div className="markdown flex min-h-dvh flex-col place-self-center overflow-hidden pt-8 pb-20">
       <AcademicContent />
     </div>
   );
