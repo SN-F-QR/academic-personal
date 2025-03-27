7358faf45ec5630d69a2301d12225365d740f506
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-28 01:23:52 +0900

    feat: add inline nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router";
 import LocationWidget from "./LocationWidget";
+import React from "react";
 
 const NavBar: React.FC = () => {
-  const navButtons = ["Academic", "About", "Links"].map((name, index) => {
-    let path = name.toLowerCase();
-    if (index === 0) {
-      path = "";
-    }
-    return <NavButton key={`nav-button-${name}`} name={name} path={path} />;
-  });
+  // const navButtons = ["Academic", "About", "Links"].map((name, index) => {
+  //   let path = name.toLowerCase();
+  //   if (index === 0) {
+  //     path = "";
+  //   }
+  //   return <NavButton key={`nav-button-${name}`} name={name} path={path} />;
+  // });
 
   return (
-    <div className="sticky top-8 mx-8 flex h-full max-w-54 flex-col justify-items-center space-y-4 font-sans">
+    <div className="sticky top-8 mx-8 hidden h-full max-w-54 flex-col justify-items-center space-y-4 font-sans lg:flex">
       <img
         src="./avatar.png"
         alt="avatar of Yang Zhan"
@@ -21,11 +22,43 @@ const NavBar: React.FC = () => {
         Yang Zhan
       </h1>
       <nav className="flex flex-col space-y-2 text-xl font-light text-gray">
-        {navButtons}
+        <NavButtonList names={["Academic", "About", "Links"]} />
       </nav>
       <LocationWidget />
     </div>
   );
+};
+
+export const InlineNavBar: React.FC = () => {
+  return (
+    <div className="flex w-full flex-1 flex-col space-y-4 pb-4 lg:hidden">
+      <div className="flex items-center space-x-4">
+        <img
+          src="./avatar.png"
+          alt="avatar of Yang Zhan"
+          className="mt-4 h-32 w-32 rounded-full border border-black"
+        />
+        <h1 className="font-sans text-3xl font-bold">Yang Zhan</h1>
+      </div>
+      <nav className="flex space-x-2 text-xl font-light text-gray">
+        <NavButtonList names={["Academic", "About", "Links"]} />
+      </nav>
+    </div>
+  );
+};
+
+const NavButtonList: React.FC<{
+  names: string[];
+}> = ({ names }) => {
+  const navButtons = names.map((name, index) => {
+    let path = name.toLowerCase();
+    if (index === 0) {
+      path = "";
+    }
+    return <NavButton key={`nav-button-${name}`} name={name} path={path} />;
+  });
+
+  return navButtons;
 };
 
 const NavButton: React.FC<{
@@ -36,11 +69,11 @@ const NavButton: React.FC<{
     <NavLink
       to={path}
       className={({ isActive }) =>
-        `h-10 w-54 cursor-pointer rounded-xl ${isActive ? "bg-purple-mid text-purple-strong shadow-md" : "bg-[#f5f4ff]"}`
+        `h-10 max-w-54 flex-1 grow cursor-pointer rounded-xl ${isActive ? "bg-purple-mid text-purple-strong shadow-md" : "bg-[#f5f4ff]"}`
       }
     >
       <div className="flex h-full">
-        <span className="place-self-center px-4">{name}</span>
+        <span className="min-w-4 place-self-center px-4">{name}</span>
       </div>
     </NavLink>
   );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Academic from "./Academic";
 import Links from "./Links";
-import NavBar from "../components/NavBar";
+import NavBar, { InlineNavBar } from "../components/NavBar";
 import About from "./About";
 
 function App() {
   const pageStyle =
-    "markdown flex min-h-dvh w-full flex-col place-self-center overflow-hidden pt-8 pb-20 mr-8 lg:ml-16 md:max-w-2xl";
+    "markdown flex min-h-dvh w-full flex-col place-self-center overflow-hidden lg:mr-8 lg:ml-16 md:max-w-2xl md:min-w-2xl";
 
   return (
     <BrowserRouter>
-      <div className="h-full w-full bg-purple-light">
-        <div className="flex h-full place-self-center xl:mr-40">
+      <div className="h-full w-full bg-purple-light p-4">
+        <div className="h-full place-self-center pt-8 pb-20 lg:flex xl:mr-40">
           <NavBar />
+          <InlineNavBar />
           <div>
             <Routes>
               <Route path="/" element={<Academic pageStyle={pageStyle} />} />
