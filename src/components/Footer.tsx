3f66957478d21f892996cee156f9160b134b1c3a
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-04-16 23:40:53 +0800

    doc: change skills to icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="text-xs font-light text-gray">
-      <p>Last updated 2025-04-13</p>
+    <footer className="font-sans text-xs font-light text-gray">
+      <p>Last updated 2025-04-16</p>
       <span className="mt-1 inline-flex">
         <p>&copy; Designed by Yang Zhan. Code soon available at</p>
         <FaGithub className="ml-1 size-3 place-self-center text-purple-strong" />
