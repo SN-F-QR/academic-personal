a5510f8b7b77b8f29fc5b4e8511018695276b2b1
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-04-18 20:40:52 +0800

    fix: reduce size for Location widget

--- src/components/LocationWidget.tsx
+++ src/components/LocationWidget.tsx
@@ -1,8 +1,8 @@
 const LocationWidget = () => {
   return (
     <a href="https://www.google.com/maps/place/Tokyo">
-      <div className="relative md:h-32 md:w-full">
-        <div className="absolute top-[30%] left-[35%] size-8">
+      <div className="relative h-28 md:w-full">
+        <div className="absolute top-[35%] left-[35%] size-8">
           <div className="relative h-full w-full">
             <div className="absolute top-1/4 left-1/4 grid size-6 animate-[ping_3s_cubic-bezier(0,0,0.2,1)infinite] rounded-full bg-sky-500 opacity-75" />
             <div className="absolute top-1/4 left-1/4 grid size-6 place-content-center rounded-full bg-white">
@@ -15,7 +15,7 @@ const LocationWidget = () => {
           alt="google map of Tokyo"
           className="w-54 rounded-3xl"
         />
-        <div className="absolute bottom-1/5 left-1/12 h-6 w-14">
+        <div className="absolute bottom-1/12 left-1/12 h-6 w-14">
           <div className="relative flex h-full w-full items-center justify-center">
             <div className="absolute h-full w-full translate-x-0 rounded-lg bg-white/70"></div>
             <p className="absolute text-sm text-purple-strong opacity-100">
