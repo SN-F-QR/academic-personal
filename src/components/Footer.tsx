804dd96e58b1babcba9106976096eddf8dd33c9d
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-04-24 15:28:41 +0900

    docs: update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,12 @@ import GithubIcon from "/github-mark.svg";
 const Footer = () => {
   return (
     <footer className="font-sans text-xs font-light text-gray">
-      <p>Last updated 2025-04-19</p>
+      <p>Last updated 2025-04-24</p>
       <span className="mt-1 inline-flex">
-        <p>&copy; Designed by Yang Zhan. Code soon available at</p>
-        <img src={GithubIcon} className="ml-1 size-3 place-self-center" />
+        <p>&copy; Designed by Yang Zhan. Code available on</p>
+        <a href="https://github.com/SN-F-QR/academic-personal">
+          <img src={GithubIcon} className="ml-1 size-3" />
+        </a>
         <p>.</p>
       </span>
     </footer>
