4be6278fbd2bac600ea12ba35d7fda272df2c3b8
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-05-09 20:33:05 +0900

    ✨ Feat: auto generate map for location widget

--- src/components/LocationWidget.tsx
+++ src/components/LocationWidget.tsx
@@ -5,7 +5,7 @@ const LocationWidget = () => {
       href="https://www.google.com/maps/place/Tokyo"
     >
       <div className="relative">
-        <div className="absolute top-[35%] left-[35%] size-8">
+        <div className="absolute top-[35%] left-[35%] z-10 size-8">
           <div className="relative h-full w-full">
             <div className="absolute top-1/4 left-1/4 grid size-6 animate-[ping_3s_cubic-bezier(0,0,0.2,1)infinite] rounded-full bg-sky-500 opacity-75" />
             <div className="absolute top-1/4 left-1/4 grid size-6 place-content-center rounded-full bg-white">
@@ -14,9 +14,9 @@ const LocationWidget = () => {
           </div>
         </div>
         <img
-          src="./tokyo_map.png"
+          src="./generated-map.png"
           alt="google map of Tokyo"
-          className="h-28 w-full object-cover"
+          className="h-28 w-full scale-[2.5] object-contain pr-2"
         />
         <div className="absolute bottom-1/12 left-1/12 h-6 w-14">
           <div className="relative flex h-full w-full items-center justify-center">
